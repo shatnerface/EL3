463208144395fe092808eafe4de66d827be05365
shatnerface <shatnerface@gmail.com>
2016-10-08 21:52:27 -0700

    It's been a while
    Quite a few changes, i think.

--- assets/contact.php
+++ assets/contact.php
@@ -1,38 +1,48 @@
 <?php
+//--------------------------Set these paramaters--------------------------
 
-// Email address verification
-function isEmail($email) {
-	return filter_var($email, FILTER_VALIDATE_EMAIL);
+// Subject of email sent to you.
+$subject = 'My Form Response';
+
+// Your email address. This is where the form information will be sent.
+$emailadd = 'you@yourdomain.com;
+
+// From email address. This is who the form information will be sent from.
+$email_from = $_POST['email'];
+
+// Where to redirect after form is processed.
+$url = 'javascript://history.go(-1)';
+
+// Makes all fields required. If set to '1' no field can not be empty. If set to '0' any or all fields can be empty.
+$req = '1';
+
+// --------------------------Do not edit below this line--------------------------
+$text = "Results from Contact form:\n\n";
+$space = ' ';
+$line = '
+';
+foreach ($_POST as $key => $value)
+{
+if ($req == '1')
+{
+if ($value == '')
+{echo "$key is empty";die;}
 }
-
-if($_POST) {
-
-    // Enter the email where you want to receive the message
-    $emailTo = 'el3company@gmail.com';
-
-    $clientEmail = addslashes(trim($_POST['email']));
-    $subject = addslashes(trim($_POST['subject']));
-    $message = addslashes(trim($_POST['message']));
-
-    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
-
-    if(!isEmail($clientEmail)) {
-        $array['emailMessage'] = 'Invalid email!';
-    }
-    if($subject == '') {
-        $array['subjectMessage'] = 'Empty subject!';
-    }
-    if($message == '') {
-        $array['messageMessage'] = 'Empty message!';
-    }
-    if(isEmail($clientEmail) && $subject != '' && $message != '') {
-        // Send email
-		$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-		mail($emailTo, $subject . " (bootstrap contact form)", $message, $headers);
-    }
-
-    echo json_encode($array);
-
+$j = strlen($key);
+if ($j >= 22)
+{echo "Name of form element $key cannot be longer than 22 characters";die;}
+$j = 22 - $j;
+for ($i = 1; $i <= $j; $i++)
+{$space .= ' ';}
+$value = str_replace('\n', "$line", $value);
+$conc = "{$key}:$space{$value}$line";
+$text .= $conc;
+$space = ' ';
 }
-
+// if the 'Gotcha' field is empty 
+if(isset($_POST['gotcha']) && $_POST['gotcha'] == ''){
+     // then send the form to your email
+mail($emailadd, $subject, $text, 'From: '.$email_from.'');
+echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+}
 ?>
